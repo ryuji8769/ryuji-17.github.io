a5e47213f77cb81b904a4a17c9daa7905ad457e7
ryuji8769 <84433849+ryuji8769@users.noreply.github.com>
2021-05-21 14:35:56 +0900

    Create jquery-3.6.0.minのコピー.js

---
+++ jquery-3.6.0.minのコピー.js
@@ -0,0 +1,16 @@
+ <script>
+
+       $(function(){
+           $('.toggle').click(function() {
+               $(this).toggleClass('active');
+   
+               if ($(this).hasClass('active')) {
+                   $ ('.gnav').addClass('active');
+               } else {
+                   $('.gnav').removeClass('active');
+               }
+       
+           });
+       }); 
+   
+    </script>
